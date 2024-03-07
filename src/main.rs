0cb01f332bbce0577628f3c4941b8cdb02a88a8e
007 <dr007cc@gmail.com>
2024-03-07 19:03:49 +0330

    feat: init rust

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("hello from dozar")
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+    .bind(("127.0.0.1", 7200))?
+    .run()
+    .await
+}
