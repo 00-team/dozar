b80a5731e9af9fafa8c8ec5f5113d87a000e4438
007 <dr007cc@gmail.com>
2024-03-11 15:40:59 +0330

    feat: add get_random utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 pub fn phone_validator(phone: &str) -> bool {
     if phone.len() != 11 || !phone.starts_with("09") {
         return false;
@@ -12,4 +14,14 @@ pub fn phone_validator(phone: &str) -> bool {
 
 pub fn now() -> i64 {
     chrono::Local::now().timestamp()
+}
+
+pub fn get_random_string(charset: &[u8], len: usize) -> String {
+    let mut rng = rand::thread_rng();
+    (0..len).map(|_| charset[rng.gen_range(0..charset.len())] as char).collect()
+}
+
+pub fn get_random_bytes(len: usize) -> String {
+    let mut rng = rand::thread_rng();
+    hex::encode((0..len).map(|_| rng.gen::<u8>()).collect::<Vec<u8>>())
 }
