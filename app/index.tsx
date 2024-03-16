97bd6f698a3a5d85414153276e4f431412972df3
sadra <sadrataghavi1383@gmail.com>
2024-03-16 14:54:04 +0330

    feat(app)

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,18 @@
+import { Route, Router, Routes } from '@solidjs/router'
+import { lazy } from 'solid-js'
 import { render } from 'solid-js/web'
 
-render(() => <div>hi</div>, document.getElementById('root'))
+const Home = lazy(() => import('./pages/home'))
+
+render(
+    () => (
+        <>
+            <Router>
+                <Routes>
+                    <Route path='/' component={Home}></Route>
+                </Routes>
+            </Router>
+        </>
+    ),
+    document.getElementById('root')
+)
