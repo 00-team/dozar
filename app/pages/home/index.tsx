16c9fb2ad0e8b5d454d86a687c26044b686bca15
sadra <sadrataghavi1383@gmail.com>
2024-03-19 14:40:33 +0330

    feat(home)

--- app/pages/home/index.tsx
+++ app/pages/home/index.tsx
@@ -2,7 +2,14 @@ import { Component } from 'solid-js'
 import './style/home.scss'
 
 const Home: Component<{}> = props => {
-    return <main class='home'></main>
+    return (
+        <main class='home'>
+            <header class='home-header section_title'>
+                <object data='/dozar.svg'></object>
+                دوزار
+            </header>
+        </main>
+    )
 }
 
 export default Home
