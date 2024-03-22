7979d5bb7ae9f9ee822cb45c39ce7311616ca3d8
sadra <sadrataghavi1383@gmail.com>
2024-03-22 23:36:51 +0330

    feat(home)

--- app/pages/home/index.tsx
+++ app/pages/home/index.tsx
@@ -1,10 +1,24 @@
+import { Typing } from '!/components/typing'
 import { Component } from 'solid-js'
 import './style/home.scss'
 
 const Home: Component<{}> = props => {
     return (
         <main class='home'>
-            <section class='hero-section'>hero</section>
+            <section class='hero-section'>
+                <header class='hero-header section_title'>
+                    <object data='/dozar.svg' type=''></object>
+                    <p>دوزار</p>
+                </header>
+                <p class='title'>
+                    {/* جایی برای کشف <span>گنجینه‌</span> های مخفی و{' '}
+                    <span>خرید</span> های با ارزش */}
+                    <Typing
+                        speed={75}
+                        sentence='جایی برای کشف گنجینه های مخفی و خرید های با ارزش'
+                    />
+                </p>
+            </section>
         </main>
     )
 }
