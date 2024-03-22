6c030a060a1cc8ceab697eba9d0abab7b7af7304
sadra <sadrataghavi1383@gmail.com>
2024-03-23 00:07:34 +0330

    feat(hopme)

--- app/pages/home/index.tsx
+++ app/pages/home/index.tsx
@@ -1,23 +1,38 @@
 import { Typing } from '!/components/typing'
-import { Component } from 'solid-js'
+import { Component, onMount } from 'solid-js'
 import './style/home.scss'
 
 const Home: Component<{}> = props => {
+    let itemsWrapper: null | HTMLElement
+    let titlesWrapper: null | HTMLElement
+
+    onMount(() => {
+        itemsWrapper = document.querySelector('.items-wrapper')
+        titlesWrapper = document.querySelector('.titles-wrapper')
+    })
+
     return (
         <main class='home'>
             <section class='hero-section'>
-                <header class='hero-header section_title'>
-                    <object data='/dozar.svg' type=''></object>
-                    <p>دوزار</p>
-                </header>
-                <p class='title'>
-                    {/* جایی برای کشف <span>گنجینه‌</span> های مخفی و{' '}
+                <div class='titles-wrapper'>
+                    <header class='hero-header section_title'>
+                        <object data='/dozar.svg' type=''></object>
+                        <p>دوزار</p>
+                    </header>
+                    <p class='title'>
+                        {/* جایی برای کشف <span>گنجینه‌</span> های مخفی و{' '}
                     <span>خرید</span> های با ارزش */}
-                    <Typing
-                        speed={75}
-                        sentence='جایی برای کشف گنجینه های مخفی و خرید های با ارزش'
-                    />
-                </p>
+                        <Typing
+                            speed={75}
+                            sentence='جایی برای کشف گنجینه های مخفی و خرید های با ارزش'
+                            onFinish={() => {
+                                titlesWrapper.className += ' active'
+                                itemsWrapper.className += ' active'
+                            }}
+                        />
+                    </p>
+                </div>
+
                 <div class='items-wrapper'>
                     <HomeItem />
                     <HomeItem />
