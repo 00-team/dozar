1e32542db8ba6b305a045c02cd608bf2f08708e0
sadra <sadrataghavi1383@gmail.com>
2024-04-01 23:03:58 +0330

    feat( home)

--- app/pages/home/index.tsx
+++ app/pages/home/index.tsx
@@ -1,4 +1,6 @@
+import ClockTick from '!/components/clockTick'
 import { Typing } from '!/components/typing'
+import { CalendarIcon, PriceIcon } from '!/icons/home'
 import { Component, onMount } from 'solid-js'
 import './style/home.scss'
 
@@ -52,7 +54,31 @@ export const HomeItem: Component = () => {
     return (
         <div class='hero-item'>
             <img src='https://picsum.photos/600/600' class='item-img' alt='' />
-            <div class='item-detail'></div>
+            <div class='item-details'>
+                <div class='detail-row item-price '>
+                    <div class='holder'>
+                        <PriceIcon />
+                        <p class='title_smaller'>قیمت پایه</p>
+                    </div>
+                    <div class='data number'>2,000</div>
+                </div>
+
+                <div class='detail-row item-init'>
+                    <div class='holder'>
+                        <CalendarIcon />
+                        <p class='title_smaller'>زمان شروع </p>
+                    </div>
+                    <div class='data'>یسشیش</div>
+                </div>
+
+                <div class='detail-row item-clock'>
+                    <div class='holder'>
+                        <ClockTick />
+                        <p class='title_smaller'>زمان باقی مانده</p>
+                    </div>
+                    <div class='data'>فلان قدر</div>
+                </div>
+            </div>
             <button class='item-goto button-basic title_small'>دیدن</button>
         </div>
     )
