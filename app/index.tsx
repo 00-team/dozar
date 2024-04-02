71826fa7891e598d76a5270c797db6311d7553d3
sadra <sadrataghavi1383@gmail.com>
2024-04-02 18:04:24 +0330

    feat(app)

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
 import { Route, Router, Routes } from '@solidjs/router'
-import { lazy, onMount } from 'solid-js'
+import { lazy, onCleanup, onMount } from 'solid-js'
 import { render } from 'solid-js/web'
+import { getDB } from './db'
+import { SHOW_ADDHOME_AGAIN } from './store/dbLabels'
 
 const Home = lazy(() => import('./pages/home'))
 const MobileHome = lazy(() => import('./pages/mobile'))
@@ -14,6 +16,25 @@ import './style/theme.scss'
 const APP_ID = 'dozar-offical-pwa'
 
 export const App = () => {
+    let addHomeElem: null | HTMLElement
+    let timeout
+
+    onMount(async () => {
+        let showAddHomeAgain = await getDB(SHOW_ADDHOME_AGAIN)
+
+        if (showAddHomeAgain === undefined) {
+            console.log('slm')
+            timeout = setTimeout(() => {
+                addHomeElem = document.querySelector('.add-home')
+
+                addHomeElem.className += ' active'
+            }, 20000)
+        }
+    })
+    onCleanup(() => {
+        clearTimeout(timeout)
+    })
+
     onMount(() => {
         if ('serviceWorker' in navigator) {
             navigator.serviceWorker.register('/sw.js')
