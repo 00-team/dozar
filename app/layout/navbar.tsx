526a75b2db0163f2ef7b645a31a552830460c35c
sadra <sadrataghavi1383@gmail.com>
2024-04-03 14:16:20 +0330

    feat(nav)

--- app/layout/navbar.tsx
+++ app/layout/navbar.tsx
@@ -3,7 +3,13 @@ import { Component } from 'solid-js'
 import './style/navbar.scss'
 
 const Navbar: Component<{}> = props => {
-    return <nav class='nav-container'></nav>
+    return (
+        <nav class='nav-container'>
+            <div class='nav-logo title'>DOZAR</div>
+            <p class='nav-title'></p>
+            <div class='nav-open'></div>
+        </nav>
+    )
 }
 
 export default Navbar
