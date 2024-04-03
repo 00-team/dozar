1b9606f8f952e0182ca616591fe7644608c126e6
sadra <sadrataghavi1383@gmail.com>
2024-04-03 15:02:15 +0330

    feat(nav)

--- app/layout/navbar.tsx
+++ app/layout/navbar.tsx
@@ -1,14 +1,52 @@
-import { Component } from 'solid-js'
+import { AuctionIcon, ContactusIcon, HomeIcon } from '!/icons/navbar'
+import { Link } from '@solidjs/router'
+import { Component, JSX } from 'solid-js'
 
 import './style/navbar.scss'
 
 const Navbar: Component<{}> = props => {
+    let isMobile = innerWidth <= 768
     return (
-        <nav class='nav-container'>
-            <div class='nav-logo title'>DOZAR</div>
-            <p class='nav-title'></p>
-            <div class='nav-open'></div>
-        </nav>
+        <>
+            {isMobile ? (
+                <></>
+            ) : (
+                <nav class='nav-container'>
+                    <div class='nav-links'>
+                        <NavLinkPC Icon={HomeIcon} link='/' title='صفحه اصلی' />
+                        <NavLinkPC
+                            Icon={ContactusIcon}
+                            link='/'
+                            title='ارتباط با ما'
+                        />
+                        <NavLinkPC
+                            Icon={AuctionIcon}
+                            link='/'
+                            title='مزایده ها'
+                        />
+                    </div>
+                    <Link href='/' class='nav-logo hero_title'>
+                        DOZAR
+                    </Link>
+                </nav>
+            )}
+        </>
+    )
+}
+
+interface NavLinkPc {
+    Icon: (P: any) => JSX.Element | JSX.Element
+    title: string
+    link: string
+}
+const NavLinkPC: Component<NavLinkPc> = ({ Icon, link, title }) => {
+    return (
+        <Link href={link} class='nav-link title_small'>
+            <div class='holder icon'>
+                <Icon />
+            </div>
+            <p class='data'>{title}</p>
+        </Link>
     )
 }
 
