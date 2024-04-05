6061744ff8ab0a44e30804f956cc09b80d7282fb
sadra <sadrataghavi1383@gmail.com>
2024-04-05 12:18:23 +0330

    feat(auctions)

--- app/pages/auctions/index.tsx
+++ app/pages/auctions/index.tsx
@@ -1,7 +1,35 @@
-import { Component } from 'solid-js'
+import { Component, createSignal } from 'solid-js'
+
+import './style/auctions.scss'
 
 const Auctions: Component<{}> = props => {
-    return <main class='auctions'></main>
+    const [filter, setFilter] = createSignal<'active' | 'inactive'>('active')
+    return (
+        <main class='auctions'>
+            <div
+                class='filter-wrapper'
+                classList={{ active: filter() === 'active' }}
+            >
+                <button
+                    class='filter title_smaller'
+                    onclick={() => filter() !== 'active' && setFilter('active')}
+                    classList={{ active: filter() === 'active' }}
+                >
+                    فعال
+                </button>
+                <button
+                    class='filter title_smaller'
+                    classList={{ active: filter() === 'inactive' }}
+                    onclick={() =>
+                        filter() !== 'inactive' && setFilter('inactive')
+                    }
+                >
+                    غیر فعال
+                </button>
+            </div>
+            <div class='auctions-wrapper'></div>
+        </main>
+    )
 }
 
 export default Auctions
