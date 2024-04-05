5a9a773bdce060338528b0838845cb12ddc2bf7c
sadra <sadrataghavi1383@gmail.com>
2024-04-05 14:59:51 +0330

    feat(auctions)

--- app/pages/auctions/index.tsx
+++ app/pages/auctions/index.tsx
@@ -1,4 +1,7 @@
+import ClockTick from '!/components/clockTick'
+import { CalendarIcon, PriceIcon } from '!/icons/home'
 import { AuctionModel } from '!/store/auction'
+import { Link } from '@solidjs/router'
 import { Component, createSignal } from 'solid-js'
 
 import './style/auctions.scss'
@@ -43,7 +46,49 @@ const Auctions: Component<{}> = props => {
 interface AuctionCardProps extends AuctionModel {}
 
 const AuctionCard: Component = () => {
-    return <div class='auction-card'></div>
+    return (
+        <div class='item'>
+            <div class='item-img-wrapper'>
+                <img
+                    class='item-img'
+                    src='https://picsum.photos/600/600'
+                    alt=''
+                />
+            </div>
+            <div class='item-wrapper'>
+                <h2 class='title item-name'>لورم ایپسوم</h2>
+
+                <div class='item-details'>
+                    <div class='detail-row item-price '>
+                        <div class='holder'>
+                            <PriceIcon />
+                            <p class='title_smaller'>قیمت پایه</p>
+                        </div>
+                        <div class='data number'>2,000</div>
+                    </div>
+
+                    <div class='detail-row item-init'>
+                        <div class='holder'>
+                            <CalendarIcon />
+                            <p class='title_smaller'>زمان شروع </p>
+                        </div>
+                        <div class='data'>یسشیش</div>
+                    </div>
+
+                    <div class='detail-row item-clock'>
+                        <div class='holder'>
+                            <ClockTick />
+                            <p class='title_smaller'>زمان باقی مانده</p>
+                        </div>
+                        <div class='data'>فلان قدر</div>
+                    </div>
+                </div>
+            </div>
+            <Link class='title_small goto' href='/auction/1'>
+                دیدن
+            </Link>
+        </div>
+    )
 }
 
 export default Auctions
