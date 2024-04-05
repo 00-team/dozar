b32e6dc9ed5cc6dab3a547ee1ac0fc8e2133c338
sadra <sadrataghavi1383@gmail.com>
2024-04-05 16:45:12 +0330

    feat(auction): page init

---
+++ app/pages/auction/index.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'solid-js'
+
+import './style/auction.scss'
+
+const Auction: Component<{}> = props => {
+    return <main class='auction'></main>
+}
+
+export default Auction
