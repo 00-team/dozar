fba59064290ca3fd5b2f90dbccd6d474a18b2ba0
sadra <sadrataghavi1383@gmail.com>
2024-04-05 11:46:37 +0330

    feat(auctions): link

--- app/index.tsx
+++ app/index.tsx
@@ -7,6 +7,7 @@ import { SHOW_ADDHOME_AGAIN } from './store/dbLabels'
 const Home = lazy(() => import('./pages/home'))
 const Navbar = lazy(() => import('./layout/Navbar'))
 const MobileHome = lazy(() => import('./pages/mobile'))
+const Auctions = lazy(() => import('./pages/auctions'))
 const AddHome = lazy(() => import('./components/addHome'))
 
 import './style/base.scss'
@@ -55,6 +56,7 @@ export const App = () => {
                 <Routes>
                     <Route path={'/'} component={Home} />
                     <Route path={'/mobile'} component={MobileHome} />
+                    <Route path={'/auctions'} component={Auctions} />
                 </Routes>
             </Router>
 
