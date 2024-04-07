9af7a7aca2e626f0802e6181df5676cd453c89d5
sadra <sadrataghavi1383@gmail.com>
2024-04-07 20:36:36 +0330

    feat(auction)

--- app/pages/auction/index.tsx
+++ app/pages/auction/index.tsx
@@ -3,7 +3,22 @@ import { Component } from 'solid-js'
 import './style/auction.scss'
 
 const Auction: Component<{}> = props => {
-    return <main class='auction'></main>
+    return (
+        <main class='auction'>
+            <aside class='item-buy'>
+                <img
+                    src='https://picsum.photos/1920/1080'
+                    alt=''
+                    class='item-img'
+                />
+                <button class='cta title_small'>خرید</button>
+                <p class='item-price title_small number'>
+                    <span>192,100,000</span>
+                </p>
+            </aside>
+            <aside class='item-details'></aside>
+        </main>
+    )
 }
 
 export default Auction
