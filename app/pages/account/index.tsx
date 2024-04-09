ee3246d8c1f2abc39ef2af3d36420f83ab6d486a
sadra <sadrataghavi1383@gmail.com>
2024-04-09 19:33:42 +0330

    feat(account)

---
+++ app/pages/account/index.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'solid-js'
+
+import './style/account.scss'
+
+const Account: Component<{}> = props => {
+    return <main class='account-container'></main>
+}
+
+export default Account
