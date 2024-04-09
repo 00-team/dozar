0a35056dcf6ac3e6b477b8eb68de64cb2385cc78
sadra <sadrataghavi1383@gmail.com>
2024-04-09 20:20:21 +0330

    feat(account)

--- app/pages/account/index.tsx
+++ app/pages/account/index.tsx
@@ -1,8 +1,16 @@
-import { Component } from 'solid-js'
+import { user } from '!/store/user'
+import { useNavigate } from '@solidjs/router'
+import { Component, onMount } from 'solid-js'
 
 import './style/account.scss'
 
 const Account: Component<{}> = props => {
+    const navigate = useNavigate()
+
+    onMount(() => {
+        if (!user.token) return navigate('/login')
+    })
+
     return <main class='account-container'></main>
 }
 
