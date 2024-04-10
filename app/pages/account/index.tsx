90311ee04181e46c806178ad389833007baab26c
sadra <sadrataghavi1383@gmail.com>
2024-04-10 15:33:14 +0330

    feat(account)

--- app/pages/account/index.tsx
+++ app/pages/account/index.tsx
@@ -1,7 +1,7 @@
 import CountUp from '!/components/countUp'
 import { WalletIcon } from '!/icons/account'
 import { user } from '!/store/user'
-import { useNavigate } from '@solidjs/router'
+import { Link, useNavigate } from '@solidjs/router'
 import { Component, onMount } from 'solid-js'
 import { MyAuctions } from './MyAuctions'
 import { MyAuctionWins } from './myAuctionWins'
@@ -19,39 +19,50 @@ const Account: Component<{}> = props => {
         if (!user.token) return navigate('/login')
     })
 
+    console.log(section)
+
     return (
         <main class='account-container'>
             <div class='account-wrapper'>
                 <section class='account-header'>
-                    <div class='wallet column'>
+                    <Link href='/account/wallet' class='wallet column'>
                         <WalletIcon />
                         <span class=' title_small'>کیف پول</span>
-                    </div>
-                    <div class='auctions column'>
+                    </Link>
+                    <Link href='/account/auctions' class='auctions column'>
                         <p class='title_small'>مزایده های شرکت کرده</p>
                         <span class='title_small number'>
                             <CountUp addTime={50} end={19} steps={2} />
                         </span>
-                    </div>
-                    <div class='auctions-win column'>
+                    </Link>
+                    <Link
+                        href='/account/auctions-win'
+                        class='auctions-win column'
+                    >
                         <p class='title_small'>مزایده های برنده شده </p>
                         <span class='title_small number'>
                             <CountUp addTime={50} end={19} steps={1} />
                         </span>
-                    </div>
+                    </Link>
                     <div class='user_name column'>
                         <p class='title_small'>صدرا ادلاعی پور</p>
-                        <button class='title_smaller'>ویرایش پروفایل</button>
+                        <Link
+                            href='/account/profile'
+                            class='title_smaller edit-btn'
+                        >
+                            ویرایش پروفایل
+                        </Link>
                     </div>
                     <div class='user_img'>
                         <img src='https://picsum.photos/300/300' alt='' />
                     </div>
                 </section>
+
                 <section class='account-body'>
                     {section ? (
                         <>
                             {section === 'wallet' && <MyWallet />}
-                            {section === 'edit' && <MyProfile />}
+                            {section === 'profile' && <MyProfile />}
                             {section === 'auctions-won' && <MyAuctionWins />}
                             {section === 'auctions' && <MyAuctions />}
                         </>
