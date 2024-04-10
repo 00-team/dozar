1db5e1a9f5de1c55e277a81773ce4bad986d41de
sadra <sadrataghavi1383@gmail.com>
2024-04-10 12:48:09 +0330

    feat(account)

--- app/pages/account/index.tsx
+++ app/pages/account/index.tsx
@@ -1,3 +1,4 @@
+import { WalletIcon } from '!/icons/account'
 import { user } from '!/store/user'
 import { useNavigate } from '@solidjs/router'
 import { Component, onMount } from 'solid-js'
@@ -11,7 +12,32 @@ const Account: Component<{}> = props => {
         if (!user.token) return navigate('/login')
     })
 
-    return <main class='account-container'></main>
+    return (
+        <main class='account-container'>
+            <section class='account-header'>
+                <div class='wallet column'>
+                    <WalletIcon />
+                    <span class=' title_small'>کیف پول</span>
+                </div>
+                <div class='auctions column'>
+                    <p class='title_small'>مزایده های شرکت کرده</p>
+                    <span class='title_small number'>19</span>
+                </div>
+                <div class='auctions-win column'>
+                    <p class='title_small'>مزایده های برنده شده </p>
+                    <span class='title_small number'>19</span>
+                </div>
+                <div class='user_name column'>
+                    <p class='title_small'>صدرا ادلاعی پور</p>
+                    <button class='title_smaller'>ویرایش پروفایل</button>
+                </div>
+                <div class='user_img'>
+                    <img src='https://picsum.photos/300/300' alt='' />
+                </div>
+            </section>
+            <section class='account-body'></section>
+        </main>
+    )
 }
 
 export default Account
