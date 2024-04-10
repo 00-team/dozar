5dc00570fcfc924b46c6975895bb53b22b5cc18b
sadra <sadrataghavi1383@gmail.com>
2024-04-10 13:23:39 +0330

    feat(account)

--- app/pages/account/index.tsx
+++ app/pages/account/index.tsx
@@ -14,28 +14,30 @@ const Account: Component<{}> = props => {
 
     return (
         <main class='account-container'>
-            <section class='account-header'>
-                <div class='wallet column'>
-                    <WalletIcon />
-                    <span class=' title_small'>کیف پول</span>
-                </div>
-                <div class='auctions column'>
-                    <p class='title_small'>مزایده های شرکت کرده</p>
-                    <span class='title_small number'>19</span>
-                </div>
-                <div class='auctions-win column'>
-                    <p class='title_small'>مزایده های برنده شده </p>
-                    <span class='title_small number'>19</span>
-                </div>
-                <div class='user_name column'>
-                    <p class='title_small'>صدرا ادلاعی پور</p>
-                    <button class='title_smaller'>ویرایش پروفایل</button>
-                </div>
-                <div class='user_img'>
-                    <img src='https://picsum.photos/300/300' alt='' />
-                </div>
-            </section>
-            <section class='account-body'></section>
+            <div class='account-wrapper'>
+                <section class='account-header'>
+                    <div class='wallet column'>
+                        <WalletIcon />
+                        <span class=' title_small'>کیف پول</span>
+                    </div>
+                    <div class='auctions column'>
+                        <p class='title_small'>مزایده های شرکت کرده</p>
+                        <span class='title_small number'>19</span>
+                    </div>
+                    <div class='auctions-win column'>
+                        <p class='title_small'>مزایده های برنده شده </p>
+                        <span class='title_small number'>19</span>
+                    </div>
+                    <div class='user_name column'>
+                        <p class='title_small'>صدرا ادلاعی پور</p>
+                        <button class='title_smaller'>ویرایش پروفایل</button>
+                    </div>
+                    <div class='user_img'>
+                        <img src='https://picsum.photos/300/300' alt='' />
+                    </div>
+                </section>
+                <section class='account-body'></section>
+            </div>
         </main>
     )
 }
