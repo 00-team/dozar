96950a044048c2e28d43a6a6eb8a4f286df56162
sadra <sadrataghavi1383@gmail.com>
2024-04-10 13:43:11 +0330

    feat(account)

---
+++ app/components/countUp.tsx
@@ -0,0 +1,43 @@
+import { Component, createSignal, onCleanup, onMount } from 'solid-js'
+
+interface CountUpProps {
+    end: number
+    steps?: number
+    addTime: number
+    cb?: () => void
+}
+
+const CountUp: Component<CountUpProps> = props => {
+    const [counter, setCounter] = createSignal(0)
+
+    let countElement: HTMLElement | null
+    let interval
+
+    onMount(() => {
+        countElement = document.querySelector('#counter-elem')
+
+        interval = setInterval(updateCount, props.addTime)
+    })
+
+    function updateCount() {
+        if (counter() < props.end) {
+            setCounter(value => (value += props.steps || 1))
+
+            if (counter() > props.end) {
+                setCounter(() => props.end)
+            }
+        } else {
+            clearInterval(interval)
+            props.cb()
+            return
+        }
+    }
+
+    onCleanup(() => {
+        clearInterval(interval)
+    })
+
+    return <span id='counter-elem'>{counter()}</span>
+}
+
+export default CountUp
