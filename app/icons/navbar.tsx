f5504899c72ec780b0c3edc8abdca869830c4242
sadra <sadrataghavi1383@gmail.com>
2024-04-10 13:23:32 +0330

    feat(nav)

--- app/icons/navbar.tsx
+++ app/icons/navbar.tsx
@@ -85,14 +85,14 @@ export const CloseIcon = P => (
     </svg>
 )
 
-export const ProfileIcon = () => (
+export const ProfileIcon = P => (
     <svg
         stroke='currentColor'
         fill='currentColor'
         stroke-width='0'
         viewBox='0 0 24 24'
-        height='1em'
-        width='1em'
+        height={P.size || 25}
+        width={P.size || 25}
         xmlns='http://www.w3.org/2000/svg'
     >
         <path fill='none' d='M0 0h24v24H0z'></path>
