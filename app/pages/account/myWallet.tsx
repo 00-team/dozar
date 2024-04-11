a2401ce7f9429f24c1bc0f52457eb9306e74ff29
sadra <sadrataghavi1383@gmail.com>
2024-04-11 13:22:51 +0330

    feat(account)

--- app/pages/account/myWallet.tsx
+++ app/pages/account/myWallet.tsx
@@ -1,5 +1,16 @@
 import { Component } from 'solid-js'
 
+import './style/mywallet.scss'
+
 export const MyWallet: Component<{}> = props => {
-    return <div></div>
+    return (
+        <section class='wallet-container'>
+            <h2 class='title'>کیف پول من</h2>
+            <div class='wallet-wrapper'>
+                <div class='wallet-card'></div>
+                <div class='wallet-input'></div>
+                <button class='cta title'>واریز</button>
+            </div>
+        </section>
+    )
 }
