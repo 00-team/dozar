d643a66f1acda3eaf4d5b40822149f5fcba8dafa
sadra <sadrataghavi1383@gmail.com>
2024-04-11 12:55:35 +0330

    feat(appp)

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,9 @@ import { Route, Router, Routes } from '@solidjs/router'
 import { lazy, onCleanup, onMount } from 'solid-js'
 import { render } from 'solid-js/web'
 import { getDB } from './db'
+import { MyAuctions } from './pages/account/MyAuctions'
+import { MyAuctionWins } from './pages/account/myAuctionWins'
+import { MyWallet } from './pages/account/myWallet'
 import { SHOW_ADDHOME_AGAIN } from './store/dbLabels'
 
 const Home = lazy(() => import('./pages/home'))
@@ -10,6 +13,7 @@ const Auctions = lazy(() => import('./pages/auctions'))
 const Auction = lazy(() => import('./pages/auction'))
 
 const Account = lazy(() => import('./pages/account'))
+
 const Login = lazy(() => import('./pages/login'))
 
 const AddHome = lazy(() => import('./components/addHome'))
@@ -62,11 +66,15 @@ export const App = () => {
                     <Route path={'/auctions'} component={Auctions} />
                     <Route path={'/auction/:slug'} component={Auction} />
 
-                    <Route path={'/account'} component={Account} />
-                    <Route path={'/account/auctions'} component={Account} />
-                    <Route path={'/account/auctions-win'} component={Account} />
-                    <Route path={'/account/wallet'} component={Account} />
-                    <Route path={'/account/profile'} component={Account} />
+                    <Route path={'/account'} component={Account}>
+                        <Route path={'/auctions'} component={MyAuctions} />
+                        <Route
+                            path={'/auctions-win'}
+                            component={MyAuctionWins}
+                        />
+                        <Route path={'/wallet'} component={MyWallet} />
+                        <Route path={'*'} component={MyAuctions} />
+                    </Route>
 
                     <Route path={'/login'} component={Login} />
                 </Routes>
