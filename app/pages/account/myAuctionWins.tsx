935dce3c54b7a410a692fb1e44748b0b9df371d5
sadra <sadrataghavi1383@gmail.com>
2024-04-11 12:58:14 +0330

    feat(account)

--- app/pages/account/myAuctionWins.tsx
+++ app/pages/account/myAuctionWins.tsx
@@ -1,5 +1,16 @@
 import { Component } from 'solid-js'
 
+import { AuctionCard } from '../auctions'
+import './style/myauctionswin.scss'
+
 export const MyAuctionWins: Component<{}> = props => {
-    return <div></div>
+    return (
+        <div class='myauctionswin'>
+            <h2 class='title'>مزایده های بنده شده</h2>
+            <div class='auctions-wrapper'>
+                <AuctionCard />
+                <AuctionCard />
+            </div>
+        </div>
+    )
 }
