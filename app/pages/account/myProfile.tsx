a0b37aad18abe8cf26809a0581f968ac385e5099
sadra <sadrataghavi1383@gmail.com>
2024-04-12 14:38:11 +0330

    feat(account)

--- app/pages/account/myProfile.tsx
+++ app/pages/account/myProfile.tsx
@@ -1,5 +1,56 @@
 import { Component } from 'solid-js'
+import { createStore } from 'solid-js/store'
+
+import './style/myprofile.scss'
+
+import defaultImg from '!/static/imgs/default_profile.webp'
+
+const IMAGE_MIMETYPE = [
+    'image/png',
+    'image/jpeg',
+    'image/jpg',
+    'image/gif',
+    'image/webm',
+]
 
 export const MyProfile: Component<{}> = props => {
-    return <div class='profile-container'></div>
+    const [profile, setProfile] = createStore({
+        username: '',
+        img: '',
+    })
+
+    return (
+        <div class='profile-container'>
+            <h2 class='title'>پروفایل من</h2>
+            <label for='profile-inp' class='profile-img'>
+                <img
+                    src={profile.img || defaultImg}
+                    decoding={'async'}
+                    loading='lazy'
+                    alt=''
+                    draggable={false}
+                />
+                <input
+                    type='file'
+                    id='profile-inp'
+                    accept='.jpg, .jpeg, .png, image/jpg, image/jpeg, image/png'
+                    onchange={e => {
+                        if (!e.target.files || !e.target.files[0]) return
+
+                        const file = e.target.files[0]
+
+                        if (!IMAGE_MIMETYPE.includes(file.type)) return
+
+                        const url = URL.createObjectURL(file)
+
+                        setProfile({
+                            img: url,
+                        })
+                    }}
+                />
+            </label>
+            <div class='profile-name'></div>
+            <button class='cta title_smaller'>تایید</button>
+        </div>
+    )
 }
